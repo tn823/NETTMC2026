bdbd33640f012b503227b24b4d7867a6fcd6111d
tn823 <trnghia823@gmail.com>
2026-04-10 14:24:02 +0700

    ex frm

---
+++ QIP/EOL/frmTMC7033.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QIP.EOL
+{
+    public partial class frmTMC7033 : UserControl
+    {
+        public frmTMC7033()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QIP/EOL/frmTMC7033_A14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QIP.EOL
+{
+    public partial class frmTMC7033_A14 : UserControl
+    {
+        public frmTMC7033_A14()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTMC7033_A14_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
