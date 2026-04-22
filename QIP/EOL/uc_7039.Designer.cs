6550e723df451a1ba911df1b7211a4c96f442928
KimQuyennn <kmquyen27122003@gmail.com>
2026-04-22 10:42:33 +0700

    uc_7039

---
+++ QIP/EOL/uc_7039.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace QIP.EOL
+{
+    partial class uc_7039
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            lblCount = new Label();
+            lblLine = new Label();
+            panel2 = new Panel();
+            pictureEdit1 = new PictureBox();
+            panel3 = new Panel();
+            lblReasonEN = new Label();
+            lblReasonVN = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureEdit1).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 198F));
+            tableLayoutPanel1.Controls.Add(lblReasonVN, 1, 2);
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(panel3, 0, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 11.0711794F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 73.25546F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 15.6733561F));
+            tableLayoutPanel1.Size = new Size(403, 293);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            tableLayoutPanel1.SetColumnSpan(panel1, 2);
+            panel1.Controls.Add(lblLine);
+            panel1.Controls.Add(lblCount);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(397, 26);
+            panel1.TabIndex = 0;
+            // 
+            // lblCount
+            // 
+            lblCount.Dock = DockStyle.Right;
+            lblCount.Font = new Font("Tahoma", 8.25F, FontStyle.Bold);
+            lblCount.Location = new Point(378, 0);
+            lblCount.Name = "lblCount";
+            lblCount.Size = new Size(19, 26);
+            lblCount.TabIndex = 0;
+            lblCount.Text = "0";
+            lblCount.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblLine
+            // 
+            lblLine.BackColor = Color.LightSkyBlue;
+            lblLine.Dock = DockStyle.Fill;
+            lblLine.Font = new Font("Calibri", 20.25F, FontStyle.Bold);
+            lblLine.ForeColor = Color.DarkOliveGreen;
+            lblLine.Location = new Point(0, 0);
+            lblLine.Name = "lblLine";
+            lblLine.Size = new Size(378, 26);
+            lblLine.TabIndex = 1;
+            lblLine.Text = "labelControl1";
+            lblLine.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            tableLayoutPanel1.SetColumnSpan(panel2, 2);
+            panel2.Controls.Add(pictureEdit1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(3, 35);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(397, 208);
+            panel2.TabIndex = 1;
+            // 
+            // pictureEdit1
+            // 
+            pictureEdit1.BackColor = Color.SkyBlue;
+            pictureEdit1.Dock = DockStyle.Fill;
+            pictureEdit1.Location = new Point(0, 0);
+            pictureEdit1.Name = "pictureEdit1";
+            pictureEdit1.Size = new Size(397, 208);
+            pictureEdit1.TabIndex = 0;
+            pictureEdit1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(lblReasonEN);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(3, 249);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(199, 41);
+            panel3.TabIndex = 2;
+            // 
+            // lblReasonEN
+            // 
+            lblReasonEN.BackColor = Color.LightSkyBlue;
+            lblReasonEN.Dock = DockStyle.Fill;
+            lblReasonEN.Font = new Font("VNI-Times", 12F);
+            lblReasonEN.Location = new Point(0, 0);
+            lblReasonEN.Name = "lblReasonEN";
+            lblReasonEN.Size = new Size(199, 41);
+            lblReasonEN.TabIndex = 0;
+            lblReasonEN.Text = "labelControl1";
+            lblReasonEN.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblReasonVN
+            // 
+            lblReasonVN.BackColor = Color.LightSkyBlue;
+            lblReasonVN.Dock = DockStyle.Fill;
+            lblReasonVN.Font = new Font("VNI-Times", 12F);
+            lblReasonVN.Location = new Point(208, 246);
+            lblReasonVN.Name = "lblReasonVN";
+            lblReasonVN.Size = new Size(192, 47);
+            lblReasonVN.TabIndex = 3;
+            lblReasonVN.Text = "labelControl1";
+            lblReasonVN.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // uc_7039
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Name = "uc_7039";
+            Size = new Size(403, 293);
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureEdit1).EndInit();
+            panel3.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private Label lblLine;
+        private Label lblCount;
+        private Panel panel2;
+        private PictureBox pictureEdit1;
+        private Panel panel3;
+        private Label lblReasonVN;
+        private Label lblReasonEN;
+    }
+}
