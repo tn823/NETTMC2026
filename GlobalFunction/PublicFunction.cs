d68dc77850bf69725ca0fbf44b4566e026c95f60
KimQuyennn <kmquyen27122003@gmail.com>
2026-05-05 12:38:56 +0700

    aa

--- GlobalFunction/PublicFunction.cs
+++ GlobalFunction/PublicFunction.cs
@@ -457,13 +457,13 @@ namespace GlobalFunction
                     if (ip.ToString() == "192.168.1.55" || ip.ToString() == "192.168.1.196" || ip.ToString() == "192.168.0.118" || ip.ToString() == "192.168.1.150")
                     {
                         //return ip.ToString();
-                        //return "192.168.0.85";
+                        return "192.168.0.85";
                         //return "192.168.1.197";
                         //return "192.168.17.241";
                         //return "192.168.0.236";
                         //return "192.168.3.174";
                         //return "192.168.0.105";
-                        return "192.168.31.62";
+                        //return "192.168.31.62";
                     }
                     else
                     {
